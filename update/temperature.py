463d6ef36b5d0f75566bc10add4a2513be4ab81a
DEX3D <61697687+burasate@users.noreply.github.com>
2020-11-06 13:36:39 +0700

    Update temperature.py

--- update/temperature.py
+++ update/temperature.py
@@ -19,7 +19,7 @@ def getRawData(*_):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         #print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        #data = {'temperature':temperature,'humidity':humidity}
+        data = {'temperature':temperature,'humidity':humidity}
         return data
     else:
         print ('DHT22 Failed to get reading. Try again!')
